44127d1c20a20f00fb3f4967f10451fffaaba0ef
Adrian Lopez <adrianlopezpazos@gmail.com>
2022-05-08 12:01:34 -0500

    commit

--- src/redux/slices/posts/postSlices.js
+++ src/redux/slices/posts/postSlices.js
@@ -36,7 +36,7 @@ export const createPostAction = createAsyncThunk(
    
 
       dispatch(resetAction());
-      return data.toLowerCase();
+      return data;
     } catch (error) {
       if (!error?.response) throw error;
       return rejectWithValue(error?.response?.data);

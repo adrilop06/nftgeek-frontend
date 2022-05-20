6937df2b8709a175ae0a7f9982770f68e6743628
Adrian Lopez <adrianlopezpazos@gmail.com>
2022-05-20 14:08:32 -0500

    commit

--- src/redux/slices/posts/postSlices.js
+++ src/redux/slices/posts/postSlices.js
@@ -177,7 +177,7 @@ export const fetchPostsUserAction = createAsyncThunk(
     };
     try {
     
-      const { data } = await axios.get(`${mainUrl}/api/posts?user=${user}`, config);
+      const { data } = await axios.get(`${mainUrl}/api/posts?user=${userAuth}`, config);
         return data;
       
     } catch (error) {

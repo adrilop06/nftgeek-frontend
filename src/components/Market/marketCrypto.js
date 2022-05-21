2518826b80551fc22248d9f3d53c3cfa9620d06a
Adrian Lopez <adrianlopezpazos@gmail.com>
2022-05-21 13:38:13 -0500

    commit

--- src/components/Market/marketCrypto.js
+++ src/components/Market/marketCrypto.js
@@ -123,109 +123,65 @@ const MarketCrypto =  () =>{
             </div>
             
             <div className="overflow-x-auto sm:-mx-6 lg:-mx-8 block lg:hidden">
-                <div class="accordion" id="accordionExample">
-                    <div class="accordion-item bg-white border border-gray-200">
-                        <h2 class="accordion-header mb-0" id="headingOne">
-                        <button class="
-                            accordion-button
-                            relative
-                            flex
-                            items-center
-                            w-full
-                            py-4
-                            px-5
-                            text-base text-gray-800 text-left
-                            bg-white
-                            border-0
-                            rounded-none
-                            transition
-                            focus:outline-none
-                        " type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true"
-                            aria-controls="collapseOne">
-                            Accordion Item #1
-                        </button>
-                        </h2>
-                        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
-                        data-bs-parent="#accordionExample">
-                        <div class="accordion-body py-4 px-5">
-                            <strong>This is the first item's accordion body.</strong> It is shown by default,
-                            until the collapse plugin adds the appropriate classes that we use to style each
-                            element. These classes control the overall appearance, as well as the showing and
-                            hiding via CSS transitions. You can modify any of this with custom CSS or overriding
-                            our default variables. It's also worth noting that just about any HTML can go within
-                            the <code>.accordion-body</code>, though the transition does limit overflow.
-                        </div>
+            
+                <div id="accordion-collapse" data-accordion="collapse">
+                    <h2 id="accordion-collapse-heading-1">
+                        <button type="button" class="flex items-center focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 justify-between p-5 w-full font-medium text-left border border-gray-200 dark:border-gray-700 border-b-0 text-gray-900 dark:text-white bg-gray-100 dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-t-xl" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
+                <span>What is Flowbite?</span>
+                <svg data-accordion-icon class="w-6 h-6 shrink-0 rotate-180" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </button>
+                    </h2>
+                    <div id="accordion-collapse-body-1" aria-labelledby="accordion-collapse-heading-1">
+                        <div class="p-5 border border-gray-200 dark:border-gray-700 dark:bg-gray-900 border-b-0">
+                            <p class="mb-2 text-gray-500 dark:text-gray-400">Flowbite is an open-source library of interactive
+                                components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.</p>
+                            <p class="text-gray-500 dark:text-gray-400">Check out this guide to learn how to <a
+                                    href="https://flowbite.com/docs/getting-started/introduction/" target="_blank"
+                                    class="text-blue-600 dark:text-blue-500 hover:underline">get started</a> and start developing
+                                websites even faster with components on top of Tailwind CSS.</p>
                         </div>
                     </div>
-                    <div class="accordion-item bg-white border border-gray-200">
-                        <h2 class="accordion-header mb-0" id="headingTwo">
-                        <button class="
-                            accordion-button
-                            collapsed
-                            relative
-                            flex
-                            items-center
-                            w-full
-                            py-4
-                            px-5
-                            text-base text-gray-800 text-left
-                            bg-white
-                            border-0
-                            rounded-none
-                            transition
-                            focus:outline-none
-                        " type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false"
-                            aria-controls="collapseTwo">
-                            Accordion Item #2
-                        </button>
-                        </h2>
-                        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
-                        data-bs-parent="#accordionExample">
-                        <div class="accordion-body py-4 px-5">
-                            <strong>This is the second item's accordion body.</strong> It is hidden by default,
-                            until the collapse plugin adds the appropriate classes that we use to style each
-                            element. These classes control the overall appearance, as well as the showing and
-                            hiding via CSS transitions. You can modify any of this with custom CSS or overriding
-                            our default variables. It's also worth noting that just about any HTML can go within
-                            the <code>.accordion-body</code>, though the transition does limit overflow.
-                        </div>
+                    <h2 id="accordion-collapse-heading-2">
+                        <button type="button" class="flex items-center focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 justify-between p-5 w-full font-medium border border-gray-200 dark:border-gray-700 border-b-0 text-left text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-2" aria-expanded="false" aria-controls="accordion-collapse-body-2">
+                <span>Is there a Figma file available?</span>
+                <svg data-accordion-icon class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </button>
+                    </h2>
+                    <div id="accordion-collapse-body-2" class="hidden" aria-labelledby="accordion-collapse-heading-2">
+                        <div class="p-5 border border-gray-200 dark:border-gray-700 border-b-0">
+                            <p class="mb-2 text-gray-500 dark:text-gray-400">Flowbite is first conceptualized and designed using the
+                                Figma software so everything you see in the library has a design equivalent in our Figma file.</p>
+                            <p class="text-gray-500 dark:text-gray-400">Check out the <a href="https://flowbite.com/figma/"
+                                    target="_blank" class="text-blue-600 dark:text-blue-500 hover:underline">Figma design system</a>
+                                based on the utility classes from Tailwind CSS and components from Flowbite.</p>
                         </div>
                     </div>
-                    <div class="accordion-item bg-white border border-gray-200">
-                        <h2 class="accordion-header mb-0" id="headingThree">
-                        <button class="
-                            accordion-button
-                            collapsed
-                            relative
-                            flex
-                            items-center
-                            w-full
-                            py-4
-                            px-5
-                            text-base text-gray-800 text-left
-                            bg-white
-                            border-0
-                            rounded-none
-                            transition
-                            focus:outline-none
-                        " type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false"
-                            aria-controls="collapseThree">
-                            Accordion Item #3
-                        </button>
-                        </h2>
-                        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
-                        data-bs-parent="#accordionExample">
-                        <div class="accordion-body py-4 px-5">
-                            <strong>This is the third item's accordion body.</strong> It is hidden by default,
-                            until the collapse plugin adds the appropriate classes that we use to style each
-                            element. These classes control the overall appearance, as well as the showing and
-                            hiding via CSS transitions. You can modify any of this with custom CSS or overriding
-                            our default variables. It's also worth noting that just about any HTML can go within
-                            the <code>.accordion-body</code>, though the transition does limit overflow.
-                        </div>
+                    <h2 id="accordion-collapse-heading-3">
+                        <button type="button" class="flex items-center border focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 border-gray-200 dark:border-gray-700 justify-between p-5 w-full font-medium text-left text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-3" aria-expanded="false" aria-controls="accordion-collapse-body-3">
+                <span>What are the differences between Flowbite and Tailwind UI?</span>
+                <svg data-accordion-icon class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </button>
+                    </h2>
+                    <div id="accordion-collapse-body-3" class="hidden" aria-labelledby="accordion-collapse-heading-3">
+                        <div class="p-5 border border-gray-200 dark:border-gray-700 border-t-0">
+                            <p class="mb-2 text-gray-500 dark:text-gray-400">The main difference is that the core components from
+                                Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product. Another
+                                difference is that Flowbite relies on smaller and standalone components, whereas Tailwind UI offers
+                                sections of pages.</p>
+                            <p class="mb-2 text-gray-500 dark:text-gray-400">However, we actually recommend using both Flowbite,
+                                Flowbite Pro, and even Tailwind UI as there is no technical reason stopping you from using the best
+                                of two worlds.</p>
+                            <p class="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
+                            <ul class="list-disc pl-5 dark:text-gray-400 text-gray-500">
+                                <li><a href="https://flowbite.com/pro/" target="_blank"
+                                        class="text-blue-600 dark:text-blue-500 hover:underline">Flowbite Pro</a></li>
+                                <li><a href="https://tailwindui.com/" rel="nofollow" target="_blank"
+                                        class="text-blue-600 dark:text-blue-500 hover:underline">Tailwind UI</a></li>
+                            </ul>
                         </div>
                     </div>
                 </div>
+        
             </div>
             
         </div>

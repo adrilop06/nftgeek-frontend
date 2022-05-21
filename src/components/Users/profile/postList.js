a29103b29bcd05f7716792ccf759e978570a3afa
Adrian Lopez <adrianlopezpazos@gmail.com>
2022-05-21 09:46:27 -0500

    commit

--- src/components/Users/profile/postList.js
+++ src/components/Users/profile/postList.js
@@ -5,36 +5,41 @@ import { postsLikes, bookmarkPostAction, fetchPostsUserAction} from "../../../re
 import { Link, useParams } from 'react-router-dom';
 import {createBookmarkAction } from '../../../redux/slices/bookmark/bookmarkSlices';
 import DateFormatter from '../../../utils/DateFormatter';
-
+import { fetchUserAction } from '../../../redux/slices/users/userSlices';
 
 const PostList = () => {
 
     const user = useSelector(state => state?.users);
-    const { userAuth } = user;
+    const { userAuth, userProfile, loading, appErr, serverErr} = user;
     const log = userAuth?._id;
   
+    
     const post = useSelector(state => state?.post);
     const { 
-        postUser, likes, mark
+        likes, mark
     } = post;
 
 
 
     const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(fetchUserAction(log))
+    }, [ log, dispatch, likes, mark]);
     
+    /*
     useEffect(() => {
         dispatch(fetchPostsUserAction(log))
-    }, [ dispatch, likes, mark]);
-
+    }, [ dispatch, ]);
+*/
     return (
         <>  
 
         <div className="grid grid-cols-1 md:grid-cols-12 gap-2 mb-5">
-            {postUser?.length <= 0 ? (
+            {userProfile?.posts?.length <= 0 ? (
                 <h1 className="text-lg text-center">Tu registro de publicaciones esta vacío</h1>
             ) :
             (
-                postUser?.map(post => (
+                userProfile?.posts?.map(post => (
             <div key={post?._id} className="flex flex-col col-span-12 md:col-span-6 lg:col-span-4 xl:col-span-3 shadow-lg rounded-lg pb-2 mt-10">
                 <Link to={`/posts/${post?.id}`}>
                     <div className="h-56 bg-cover text-center overflow-hidden rounded-lg"  title="Image post">
@@ -54,7 +59,7 @@ const PostList = () => {
                 <div className="flex items-center ml-4 mt-auto">
                     <a href="/"><img className="w-10 h-10 rounded-full mr-4" src={post?.userImage} alt="User Photo"/></a>
                     <div className="text-md w-100 flex-1">
-                        <a href="/" className="text-gray-900">{post?.user?.userName}</a>
+                        <a href="/" className="text-gray-900">{post?.userName}</a>
                         <p className="text-gray-600"><DateFormatter date={post?.createdAt}/></p>
                     </div>
                     <div className="flex items-center ml-auto mr-2 mb-0">

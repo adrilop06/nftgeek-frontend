11e0c9783df5da62d63fe45a878cb35d8ceee327
Adrian Lopez <adrianlopezpazos@gmail.com>
2022-05-21 16:01:53 -0500

    commit

--- src/components/Market/marketCrypto.js
+++ src/components/Market/marketCrypto.js
@@ -129,107 +129,111 @@ const MarketCrypto =  () =>{
                 </div>
             </div>
             
-            <div className="overflow:hidden max-w-full mx-2 block lg:hidden">
-     
-                <table className="max-w-full">
-                    <thead className="bg-white border">
-                        <tr>
-                        <th scope="col" className="text-sm font-bold text-black px-1 py-4 text-left">
-                            Nº
-                        </th>
-                        <th scope="col" className="text-sm font-bold text-black px-1 py-4 text-left">
-                            Criptomoneda
-                        </th>
-                        <th scope="col" className="text-sm font-bold text-black px-1 py-4 text-left">
-                            Precio
-                        </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {/*table with values*/}
-                        {appErr || serverErr ? (
-                            <h1>Err</h1>
-                        ) : marketList?.lenght <= 0 ? (
-                            <h1>No values Found</h1>
-                        ): (
-                            marketList?.slice(0, 100).map(values => (
-                                <Accordion>
-                                <AccordionItem>
-                                    <tr key={values?.id} className="bg-white border-b transition duration-300 ease-in-out hover:bg-blue-100">
-                                    <td className="px-4 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                                    <AccordionItemHeading>
-                                        <AccordionItemButton>
-                                            
-                                                <ChevronDownIcon className="w-4 stroke-gray-500 ml-auto"></ChevronDownIcon>
-                                            
-                                        </AccordionItemButton>
-                                    </AccordionItemHeading>
-                                    </td>
-                                    
-                                    <td className="text-sm text-gray-900 font-light px-4 py-4 whitespace-nowrap">
-                                        <div className="flex items-center ml-auto">
-                                            <img
-                                            className="w-6 mr-4"
-                                            src={values?.image}></img>
-                                            {values?.name}
-                                        </div>
-                                    </td>
-                                    <td className="text-sm text-gray-900 font-light px-4 py-4 whitespace-nowrap">
-                                        {values?.current_price.toLocaleString(undefined, { maximumFractionDigits: 2 })}
-                                    </td>
-                                  
-                                    </tr>
-                                
-                               
-                                <AccordionItemPanel>
-                                <tr key={values?.id} className="bg-white border-b transition duration-300 ease-in-out hover:bg-blue-100">
-                                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                        {values?.market_cap_change_percentage_24h < 0 ? (
-                                            <div className="flex items-center">
-                                                <p className="text-red-500 font-bold">24%: {values?.market_cap_change_percentage_24h.toLocaleString(undefined, { maximumFractionDigits: 2 })}</p>
-                                                <TrendingDownIcon className="w-4 stroke-red-500 ml-auto"/>
-                                            </div>
-                                        ):(
-                                            <div className="flex items-center ml-auto">
-                                                <p className="text-green-500 font-bold">24%: {values?.market_cap_change_percentage_24h.toLocaleString(undefined, { maximumFractionDigits: 2 })}</p>
-                                                <TrendingUpIcon className="w-4 stroke-green-500 ml-auto"/>
-                                            </div>
-                                        )}
-                                    
 
-                                    </td>
-                                </tr>
-                                <tr key={values?.id} className="bg-white border-b transition duration-300 ease-in-out hover:bg-blue-100">
-                                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                        <p> Mas bajo 24h: {values?.low_24h.toLocaleString(undefined, { maximumFractionDigits: 2 })}</p>
-                                    </td>
-                                </tr>
-                                <tr key={values?.id} className="bg-white border-b transition duration-300 ease-in-out hover:bg-blue-100">
-                                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                        <p> Mas alto 24h: {values?.high_24h.toLocaleString(undefined, { maximumFractionDigits: 2 })}</p>
-                                    </td>
-                                </tr>
-                                <tr key={values?.id} className="bg-white border-b transition duration-300 ease-in-out hover:bg-blue-100">
-                                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                        <p> Valor 24h: {values?.total_volume.toLocaleString(undefined, { maximumFractionDigits: 2 })}</p>
-                                    </td>
-                                </tr>
-                                <tr key={values?.id} className="bg-white border-b transition duration-300 ease-in-out hover:bg-blue-100">
-                                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                        <p> Valor mercado: {values?.market_cap.toLocaleString(undefined, { maximumFractionDigits: 2 })}</p>
-                                    </td>
-                                </tr>
+                
+
+            <div className="overflow:hidden max-w-full mx-2 block lg:hidden">
+                <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                <div className="overflow-hidden">
+                    <table className="max-w-full">
+                        <thead className="bg-white border">
+                            <tr>
+                            <th scope="col" className="text-sm font-bold text-black px-1 py-4 text-left">
+                                Nº
+                            </th>
+                            <th scope="col" className="text-sm font-bold text-black px-1 py-4 text-left">
+                                Criptomoneda
+                            </th>
+                            <th scope="col" className="text-sm font-bold text-black px-1 py-4 text-left">
+                                Precio
+                            </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/*table with values*/}
+                            {appErr || serverErr ? (
+                                <h1>Err</h1>
+                            ) : marketList?.lenght <= 0 ? (
+                                <h1>No values Found</h1>
+                            ): (
+                                marketList?.slice(0, 100).map(values => (
+                                    <Accordion>
+                                    <AccordionItem>
+                                        <tr key={values?.id} className="bg-white border-b transition duration-300 ease-in-out hover:bg-blue-100">
+                                        <td className="px-4 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                        <AccordionItemHeading>
+                                            <AccordionItemButton>
+                                                
+                                                    <ChevronDownIcon className="w-4 stroke-gray-500 ml-auto"></ChevronDownIcon>
+                                                
+                                            </AccordionItemButton>
+                                        </AccordionItemHeading>
+                                        </td>
+                                        
+                                        <td className="text-sm text-gray-900 font-light px-4 py-4 whitespace-nowrap">
+                                            <div className="flex items-center ml-auto">
+                                                <img
+                                                className="w-6 mr-4"
+                                                src={values?.image}></img>
+                                                {values?.name}
+                                            </div>
+                                        </td>
+                                        <td className="text-sm text-gray-900 font-light px-4 py-4 whitespace-nowrap">
+                                            {values?.current_price.toLocaleString(undefined, { maximumFractionDigits: 2 })}
+                                        </td>
+                                    
+                                        </tr>
+                                    
                                 
-                                </AccordionItemPanel>
-                                
-                            </AccordionItem>
-                            </Accordion>
-                            ))
-                        )}
-                    </tbody>
-                </table>
-            
-              
+                                    <AccordionItemPanel>
+                                    <tr key={values?.id} className="bg-white border-b transition duration-300 ease-in-out hover:bg-blue-100">
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            {values?.market_cap_change_percentage_24h < 0 ? (
+                                                <div className="flex items-center">
+                                                    <p className="text-red-500 font-bold">24%: {values?.market_cap_change_percentage_24h.toLocaleString(undefined, { maximumFractionDigits: 2 })}</p>
+                                                    <TrendingDownIcon className="w-4 stroke-red-500 ml-auto"/>
+                                                </div>
+                                            ):(
+                                                <div className="flex items-center ml-auto">
+                                                    <p className="text-green-500 font-bold">24%: {values?.market_cap_change_percentage_24h.toLocaleString(undefined, { maximumFractionDigits: 2 })}</p>
+                                                    <TrendingUpIcon className="w-4 stroke-green-500 ml-auto"/>
+                                                </div>
+                                            )}
+                                        
+
+                                        </td>
+                                    </tr>
+                                    <tr key={values?.id} className="bg-white border-b transition duration-300 ease-in-out hover:bg-blue-100">
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            <p> Mas bajo 24h: {values?.low_24h.toLocaleString(undefined, { maximumFractionDigits: 2 })}</p>
+                                        </td>
+                                    </tr>
+                                    <tr key={values?.id} className="bg-white border-b transition duration-300 ease-in-out hover:bg-blue-100">
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            <p> Mas alto 24h: {values?.high_24h.toLocaleString(undefined, { maximumFractionDigits: 2 })}</p>
+                                        </td>
+                                    </tr>
+                                    <tr key={values?.id} className="bg-white border-b transition duration-300 ease-in-out hover:bg-blue-100">
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            <p> Valor 24h: {values?.total_volume.toLocaleString(undefined, { maximumFractionDigits: 2 })}</p>
+                                        </td>
+                                    </tr>
+                                    <tr key={values?.id} className="bg-white border-b transition duration-300 ease-in-out hover:bg-blue-100">
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            <p> Valor mercado: {values?.market_cap.toLocaleString(undefined, { maximumFractionDigits: 2 })}</p>
+                                        </td>
+                                    </tr>
+                                    
+                                    </AccordionItemPanel>
+                                    
+                                </AccordionItem>
+                                </Accordion>
+                                ))
+                            )}
+                        </tbody>
+                    </table>
+                </div>                            
+            </div>
         
             </div>
             

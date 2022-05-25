c4d08ade323158b8415da3fc67a77f8c3dce1052
Adrian Lopez <adrianlopezpazos@gmail.com>
2022-05-25 17:08:12 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import './index.css';
 import App from './App';
 import store from './redux/store/store';
 import 'flowbite';
+import '../src/styles/style.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <App/>
   </Provider>
 );
 
